64edad0e6f537e1684ed77a48263b132c4dc546a
juanfmange <juanfmange@gmail.com>
2022-09-05 22:52:04 -0500

    added yargs to bases-node at the package.json

--- bases-node/app.js
+++ bases-node/app.js
@@ -1,6 +1,22 @@
 const { crearArchvio } =require('./helpers/multiplicar');
+const argv = require('yargs').argv;
+
+
+
 console.clear();
 
-const a = 7;
+console.log(process.argv);
+console.log(argv);
 
-crearArchvio(a);
+console.log('base: yargs', argv.base);
+
+
+
+// const [ , , arg3 = 'base=5'] = process.argv;
+// const [ , base=5] = arg3.split('=');
+
+//const a = 7;
+
+// crearArchvio(base) 
+  // .then( nombreArchivo => console.log(nombreArchivo, 'creado'))
+  // .catch(err => console.log(err));
--- hola-mundo/app.js
+++ hola-mundo/app.js
@@ -5,3 +5,9 @@ nombre = "paco";
 console.log(nombre);
 
 console.log('hola mundo!');
+
+const apellido = "mange";
+const segundo = 'saga';
+console.log(segundo);
+console.log(apellido);
+console.log("hola mundo!!!");
